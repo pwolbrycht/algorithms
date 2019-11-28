7e8b1a40d0c1098f08e15368670f150df64a20bd
Paweł Olbrycht <sailor_mbp@sailor-mbp.local>
2019-11-28 16:21:27 +0100

    first commit

---
+++ parallel_sort/cmake-build-debug/CMakeFiles/parallel_sort.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parallel_sort.dir/main.cpp.o"
+  "parallel_sort"
+  "parallel_sort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parallel_sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
